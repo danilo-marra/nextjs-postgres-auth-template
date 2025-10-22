b382bb8671576e177a40e8e78ec5c29e81fe8814
Danilo Marra <danilo2311@gmail.com>
2025-10-22 13:47:57 -0300

    feat: add `activation.findOneValidById()` and orchestrator.extractUUID

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -1,4 +1,5 @@
-import activation from "models/activation.js";
+import webserver from "infra/webserver";
+import activation from "models/activation";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
@@ -45,16 +46,22 @@ describe("Use case: Registration flow (all successful)", () => {
   test("Receive activation email", async () => {
     const lastEmail = await orchestrator.getLastEmail();
 
-    const activationToken = await activation.findOneByUserId(
-      createUserResponseBody.id,
-    );
-
     expect(lastEmail.sender).toBe("<contato@fintab.com.br>");
     expect(lastEmail.recipients[0]).toBe("<registration.flow@curso.dev>");
     expect(lastEmail.subject).toBe("Ative seu cadastro no FinTab!");
     expect(lastEmail.text).toContain("RegistrationFlow");
-    expect(lastEmail.text).toContain(activationToken.id);
 
+    const activationTokenId = orchestrator.extractUUID(lastEmail.text);
+
+    expect(lastEmail.text).toContain(
+      `${webserver.origin}/cadastro/ativar/${activationTokenId}`,
+    );
+
+    const activationTokenObject =
+      await activation.findOndeValidById(activationTokenId);
+
+    expect(activationTokenObject.user_id).toBe(createUserResponseBody.id);
+    expect(activationTokenObject.used_at).toBe(null);
     console.log(lastEmail.text);
   });
 
