62a961409d54888eeec677c83d360880f3d7ea85
Danilo Marra <danilo2311@gmail.com>
2025-11-29 11:20:08 -0300

    feat: require `create:user` to access `/api/v1/users`

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -1,24 +1,34 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
 import user from "models/user.js";
-import session from "models/session";
+import activation from "models/activation.js";
+import session from "models/session.js";
 
 const router = createRouter();
 
 router.use(controller.injectAnonymousOrUser);
+router.post(controller.canRequest("create:user"), postHandler);
 router.get(controller.canRequest("read:session"), getHandler);
 
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
-  const sessionToken = request.cookies.session_id;
-
-  const sessionObject = await session.findOneValidByToken(sessionToken);
+  const sessionObject = await session.findOneValidByToken(
+    request.cookies.session_id,
+  );
   const renewedSessionObject = await session.renew(sessionObject.id);
-  controller.setSessionCookie(renewedSessionObject.token, response);
-
-  const userfound = await user.findOneById(sessionObject.user_id);
+  await controller.setSessionCookie(renewedSessionObject.token, response);
 
   response.setHeader("Cache-Control", "no-store, max-age=0, must-revalidate");
-  return response.status(200).json(userfound);
+  response.status(200).json(request.context.user);
+}
+
+async function postHandler(request, response) {
+  const userInputValues = request.body;
+  const newUser = await user.create(userInputValues);
+
+  const activationToken = await activation.create(newUser.id);
+  await activation.sendEmailToUser(newUser, activationToken);
+  response.setHeader("Cache-Control", "no-store, max-age=0, must-revalidate");
+  return response.status(201).json(newUser);
 }
